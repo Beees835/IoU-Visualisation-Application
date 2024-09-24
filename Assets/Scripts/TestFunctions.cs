5cbc0204bcc36bf8426d4e1954870ae6fc360c98
zhar0008 <zacari.harrison@gmail.com>
2024-09-24 13:53:39 +1000

    generalise area tests

--- Assets/Scripts/TestFunctions.cs
+++ Assets/Scripts/TestFunctions.cs
@@ -4,6 +4,8 @@ using System;
 public static class TestingFunctions{ 
 public static bool AreCloseEnough(float value1, float value2)
 {
-    return Math.Abs(value1 - value2) <= 0.0000001f;
+    // Need to account for floating point innaccuracy
+    float errorVal = 0.0001f;
+    return Math.Abs(value1 - value2) <= errorVal;
 }
 }
