ca384c757d4750c8bedc1052d952524ee615d94a
eung0001 <107110433+emilyungm@users.noreply.github.com>
2024-09-26 18:07:08 +1000

    Added zoom functionality (btns + slider)

---
+++ Assets/Scripts/BtnScripts/SliderScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour
+{
+
+    [SerializeField] private Slider _slider;
+    [SerializeField] private TextMeshProUGUI _sliderText;
+    public float minZoom = 5f;
+    public float maxZoom = 15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _slider.onValueChanged.AddListener((val) => {
+            _sliderText.text = Mathf.RoundToInt(val / maxZoom * 100).ToString() + "%";
+            Camera.main.orthographicSize = Mathf.Clamp(maxZoom - val, minZoom, maxZoom);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
