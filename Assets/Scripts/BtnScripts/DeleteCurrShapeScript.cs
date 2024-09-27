76795122a88ce52c6502a3bd953ed406dfed417c
eung0001 <107110433+emilyungm@users.noreply.github.com>
2024-09-27 18:08:44 +1000

    Delete curr shape btn partially working (deletes lines kind of)

---
+++ Assets/Scripts/BtnScripts/DeleteCurrShapeScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeleteCurrShapeScript : MonoBehaviour
+{
+
+    [SerializeField] private Button _deleteCurrShapeBtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _deleteCurrShapeBtn.onClick.AddListener(DeleteCurrShape);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DeleteCurrShape()
+    {
+        if (ShapeManager.Instance.AllShapes.Count > 0)
+        {
+            ShapeManager.Instance.AllShapes.RemoveAt(ShapeManager.Instance.AllShapes.Count - 1);
+            ShapeManager.Instance.CurrentShape.Points.Clear();
+            ShapeRenderer.Instance.ClearCurrentLines();
+            CanvasState.Instance.drawState = CanvasState.DrawStates.DRAW_STATE;
+            CanvasState.Instance.shapeCount -= 1;
+        }
+    }
+}
