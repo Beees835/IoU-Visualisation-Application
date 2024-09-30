c50df41c8ab0bf2be253debbb9601eb32f4de930
Charles Nierves <cnie0004@student.monash.edu>
2024-09-30 21:01:22 +1000

    fixed the repeated invalid point place error message bug

--- Assets/Scripts/InputProcessing/InputController.cs
+++ Assets/Scripts/InputProcessing/InputController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class InputController : MonoBehaviour
 {
@@ -15,9 +16,12 @@ public class InputController : MonoBehaviour
 
     public void PlacePrefabAtMousePosition()
     {
+        if (IsPointerOverUIElement()) {
+            return;
+        }
+
         // Logic to select the correct prefab
         if (CanvasState.Instance.shapeCount == 0)
-
         {
             currentPrefab = Materials.Instance.PrefabShape1;
         }
@@ -192,4 +196,24 @@ public class InputController : MonoBehaviour
             NotificationManager.Instance.ShowMessage("Cannot add new points when the two shapes are defined");
         }
     }
+
+    private bool IsPointerOverUIElement()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current)
+        {
+            position = Mouse.current.position.ReadValue()
+        };
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+        foreach (RaycastResult result in results)
+        {
+            if (result.gameObject.layer == LayerMask.NameToLayer("OverlayUI"))
+            {
+                return true; 
+            }
+        }
+
+        return false;
+    }
+
 }
