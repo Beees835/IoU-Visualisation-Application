44e81293c39b53a9757c4f20ca1e6e543a70e522
Bryan Daverel <bryan.daverel@gmail.com>
2024-09-30 13:21:31 +1000

    Zooming Fix

--- Assets/Scripts/ControlLogic/CameraController.cs
+++ Assets/Scripts/ControlLogic/CameraController.cs
@@ -1,28 +1,72 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
-    private float zoomSpeed = 1f;
-    private float panSpeed = 0.3f;
-    private float minZoom = 5f;
-    private float maxZoom = 15f;
+    [Header("Zoom Settings")]
+    [SerializeField] private float zoomSpeed = 1f;
+    [SerializeField] private float panSpeed = 0.3f;
+    [SerializeField] public float minZoom = 5f;
+    [SerializeField] public float maxZoom = 15f;
 
-    private Camera cam;
+    [Header("UI Elements")]
+    [SerializeField] private Slider zoomSlider;
+    [SerializeField] private TextMeshProUGUI zoomPercentageText;
+
+    [HideInInspector] public Camera cam;
+
+    [HideInInspector] public bool isUpdating = false;
 
     private void Awake()
     {
         cam = GetComponent<Camera>();
+
+        // Initialize slider values
+        zoomSlider.minValue = 0f;
+        zoomSlider.maxValue = 1f;
+
+        // Set initial slider value and zoom percentage text
+        UpdateZoomUI();
     }
 
-    public void HandleZoom(float scrollValue)
+    private void Update()
     {
-        float newZoom = cam.orthographicSize - scrollValue * zoomSpeed;
-        cam.orthographicSize = Mathf.Clamp(newZoom, minZoom, maxZoom);
+        // Handle mouse wheel zoom
+        float scrollData = Input.GetAxis("Mouse ScrollWheel");
+        if (scrollData != 0.0f)
+        {
+            HandleZoom(-scrollData * zoomSpeed);
+        }
+    }
+
+    public void HandleZoom(float zoomAmount)
+    {
+        float newZoom = Mathf.Clamp(cam.orthographicSize - zoomAmount, minZoom, maxZoom);
+        cam.orthographicSize = newZoom;
+
+        // Update the Zoom UI
+        UpdateZoomUI();
     }
 
     public void HandlePan(Vector2 panValue)
     {
-        // Pan values are negative so that scroll feels correct
         transform.position += new Vector3(-panValue.x * panSpeed, -panValue.y * panSpeed, 0);
+    }
+
+    public void UpdateZoomUI()
+    {
+        isUpdating = true;
+
+        float val = cam.orthographicSize;
+
+        // Calculate the zoom percentage
+        float zoomPercentage = (maxZoom - val) / (maxZoom - minZoom) * 100f;
+        zoomPercentageText.text = Mathf.RoundToInt(zoomPercentage).ToString() + "%";
+
+        // Invert the slider value calculation
+        zoomSlider.value = (maxZoom - val) / (maxZoom - minZoom);
+
+        isUpdating = false;
     }
 }
