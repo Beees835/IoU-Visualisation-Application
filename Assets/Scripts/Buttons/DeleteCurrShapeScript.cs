3108e3c3638e2466cc6c169d3f26195a52bd1513
zhar0008 <zhar0008@student.monash.edu>
2024-09-30 21:18:16 +1000

    Make it look purdy

--- Assets/Scripts/Buttons/DeleteCurrShapeScript.cs
+++ Assets/Scripts/Buttons/DeleteCurrShapeScript.cs
@@ -16,9 +16,6 @@ public class DeleteCurrShapeScript : MonoBehaviour
     {
         if (CanvasState.Instance.drawState == CanvasState.DrawStates.MODIFY_STATE)
         {
-            //NotificationManager.Instance.ShowMessage("Can't delete shape after intersection has been calculated.");
-            //Debug.Log("Can't delete shape after intersection");
-
             // Remove Intersection Shape
             IoUCalculator.Reset();
         }
