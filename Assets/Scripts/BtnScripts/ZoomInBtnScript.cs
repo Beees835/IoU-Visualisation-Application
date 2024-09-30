bd85da3c07625ccd3aa0d60b765b20865956b151
Bryan Daverel <bryan.daverel@gmail.com>
2024-09-30 13:21:31 +1000

    Zooming Fix

--- Assets/Scripts/BtnScripts/ZoomInBtnScript.cs
+++ Assets/Scripts/BtnScripts/ZoomInBtnScript.cs
@@ -1,28 +1,18 @@
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ZoomInBtnScript : MonoBehaviour
 {
+    [SerializeField] private Button zoomInBtn;
+    [SerializeField] private CameraController cameraController;
 
-    [SerializeField] private Button _zoomInBtn;
-    [SerializeField] private Slider _slider;
-    [SerializeField] private TextMeshProUGUI _sliderText;
-    public float minZoom = 5f;
-    public float maxZoom = 15f;
-
-    // Start is called before the first frame update
     void Start()
     {
-        _zoomInBtn.onClick.AddListener(ZoomIn);
+        zoomInBtn.onClick.AddListener(ZoomIn);
     }
 
     void ZoomIn()
     {
-        float val = Mathf.Clamp(Camera.main.orthographicSize - 1, minZoom, maxZoom);
-        Camera.main.orthographicSize = val;
-        _sliderText.text = Mathf.RoundToInt((maxZoom - val) / maxZoom * 100).ToString() + "%";
-        _slider.value = maxZoom - val;
+        cameraController.HandleZoom(1f); // Zoom in by a fixed amount
     }
-
 }
